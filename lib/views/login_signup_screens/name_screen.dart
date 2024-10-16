51ba5fe63f7d86b88352385f64465ecbbc12ff34
ABO <140602836+AB0-0@users.noreply.github.com>
2024-10-16 23:50:40 +0800

    Update name_screen.dart

--- lib/views/login_signup_screens/name_screen.dart
+++ lib/views/login_signup_screens/name_screen.dart
@@ -2,8 +2,14 @@ import 'package:flutter/material.dart';
 import '../../controller/step_progress_indicator.dart';
 import 'birthday_screen.dart';
 
-class NameScreen extends StatelessWidget {
+class NameScreen extends StatefulWidget {
+  @override
+  _NameScreenState createState() => _NameScreenState();
+}
+
+class _NameScreenState extends State<NameScreen> {
   final TextEditingController _nameController = TextEditingController();
+  bool _isNameValid = true;
 
   @override
   Widget build(BuildContext context) {
@@ -15,51 +21,73 @@ class NameScreen extends StatelessWidget {
             Navigator.pop(context);
           },
         ),
-        title: Text('Setup Your Profile', style: TextStyle(fontWeight: FontWeight.bold)),
+        title: Text(
+          'Setup Your Profile',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
         centerTitle: true,
-        backgroundColor : Color.fromRGBO(230, 230, 250, 100),
+        backgroundColor: Color.fromRGBO(230, 230, 250, 100),
       ),
       body: Material(
         color: Color.fromRGBO(230, 230, 250, 100),
         child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            SizedBox(height: 20),
-            StepProgressIndicator(currentStep: 1),
-            SizedBox(height: 200),
-            Text('What is your name?', style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold)),
-            SizedBox(height: 50),
-            TextField(
-              textAlign: TextAlign.center,
-              controller: _nameController,
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Color.fromRGBO(70, 80, 90, 245),
-                hintText: 'Your Name',
-                border: OutlineInputBorder(),
+          padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 16.0),
+          child: Column(
+            children: [
+              SizedBox(height: 20),
+              StepProgressIndicator(currentStep: 1),
+              SizedBox(height: 150),
+              Text(
+                'What is your name?',
+                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
               ),
-            ),
-            Spacer(),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => BirthdayScreen(name: _nameController.text)),
-                  );
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color.fromRGBO(255, 111, 97, 100),
+              SizedBox(height: 40),
+              TextField(
+                textAlign: TextAlign.center,
+                controller: _nameController,
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Color.fromRGBO(70, 80, 90, 0.2),
+                  hintText: 'Your Name',
+                  errorText: _isNameValid ? null : 'Please enter your name',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
                 ),
-                child: Text('Next', style: TextStyle(color: Colors.white70)),
               ),
-            ),
-          ],
+              Spacer(),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (_nameController.text.isEmpty) {
+                      setState(() {
+                        _isNameValid = false;
+                      });
+                    } else {
+                      setState(() {
+                        _isNameValid = true;
+                      });
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) =>
+                              BirthdayScreen(name: _nameController.text),
+                        ),
+                      );
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color.fromRGBO(255, 111, 97, 1),
+                  ),
+                  child: Text('Next', style: TextStyle(color: Colors.white)),
+                ),
+              ),
+              SizedBox(height: 20),
+            ],
+          ),
         ),
       ),
-    ),);
+    );
   }
 }
