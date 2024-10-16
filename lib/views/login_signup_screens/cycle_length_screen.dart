73a48b677bef4d85a89eaea4cbff906a4dca3010
ABO <140602836+AB0-0@users.noreply.github.com>
2024-10-16 23:57:23 +0800

    Update cycle_length_screen.dart

--- lib/views/login_signup_screens/cycle_length_screen.dart
+++ lib/views/login_signup_screens/cycle_length_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import '../../controller/csv_manager.dart';
 import '../../controller/step_progress_indicator.dart';
 import 'last_period_screen.dart';
 
@@ -26,58 +27,72 @@ class _CycleLengthScreenState extends State<CycleLengthScreen> {
             Navigator.pop(context);
           },
         ),
-        title: Text('Setup Your Profile', style: TextStyle(fontWeight: FontWeight.bold),),
+        title: Text(
+          'Setup Your Profile',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
         centerTitle: true,
-        backgroundColor: Color.fromRGBO(230, 230, 250, 100),
+        backgroundColor: Color.fromRGBO(230, 230, 250, 1),
       ),
       body: Material(
-        color: Color.fromRGBO(230, 230, 250, 100),
-        child:Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            SizedBox(height: 20),
-            StepProgressIndicator(currentStep: 6),
-            SizedBox(height: 200),
-            Text('How long does your cycle last?', style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold)),
-            SizedBox(height: 50),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Expanded(child: CupertinoPicker(
-                  itemExtent: 50,
-                  onSelectedItemChanged: (index) {
-                    setState(() {
-                      selectedCycleLength = index + 1;
-                    });
-                  },
-                  children: List.generate(100, (index) {
-                    return Center(child: Text('${index + 1}'));
-                  }),
-                ),
-                ),
-              ],
-            ),
-            Spacer(),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => LastPeriodScreen(name: widget.name)),
-                  );
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color.fromRGBO(255, 111, 97, 100),
-                ),
-                child: Text('Next', style: TextStyle(color: Colors.white70)),
+        color: Color.fromRGBO(230, 230, 250, 1),
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              SizedBox(height: 20),
+              StepProgressIndicator(currentStep: 6),
+              SizedBox(height: 20),
+              Text(
+                'How long does your cycle last?',
+                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
               ),
-            ),
-          ],
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: CupertinoPicker(
+                      itemExtent: 50,
+                      onSelectedItemChanged: (index) {
+                        setState(() {
+                          selectedCycleLength = index + 1;
+                        });
+                      },
+                      children: List.generate(cycle.length, (index) {
+                        return Center(child: Text('${index + 1} days'));
+                      }),
+                    ),
+                  ),
+                ],
+              ),
+              Spacer(),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () async {
+                    List<dynamic> cycleData = [selectedCycleLength, widget.name];
+
+                    CSVManager csvManager = CSVManager();
+                    await csvManager.addToCSV(cycleData);
+
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => LastPeriodScreen(name: widget.name),
+                      ),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color.fromRGBO(255, 111, 97, 1),
+                  ),
+                  child: Text('Next', style: TextStyle(color: Colors.white70)),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-    ),);
+    );
   }
 }
