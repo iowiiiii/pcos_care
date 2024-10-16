9874e81f45b60f479473d9996d901a63fd03d964
ABO <140602836+AB0-0@users.noreply.github.com>
2024-10-16 23:58:13 +0800

    Update period_duration_screen.dart

--- lib/views/login_signup_screens/period_duration_screen.dart
+++ lib/views/login_signup_screens/period_duration_screen.dart
@@ -1,24 +1,24 @@
-import 'dart:ui';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:table_calendar/table_calendar.dart';
 import '../../controller/csv_manager.dart';
 import '../../controller/step_progress_indicator.dart';
-import 'cycle_length_screen.dart';
+import 'finish_setup_screen.dart';
 
-class PeriodDurationScreen extends StatefulWidget {
+class LastPeriodScreen extends StatefulWidget {
   final String name;
 
-  PeriodDurationScreen({required this.name, required double bmi});
+  LastPeriodScreen({required this.name});
 
   @override
-  _PeriodDurationScreenState createState() => _PeriodDurationScreenState();
+  _LastPeriodScreenState createState() => _LastPeriodScreenState();
 }
 
-class _PeriodDurationScreenState extends State<PeriodDurationScreen> {
-  int selectedPeriodLength = 1;
+class _LastPeriodScreenState extends State<LastPeriodScreen> {
+  DateTime _focusedDay = DateTime.now();
+  List<DateTime> _selectedDays = [];
+  final CSVManager csvManager = CSVManager();
 
-  List<int> duration = List.generate(31, (index) => index + 1);
+  get userData => null;
 
   @override
   Widget build(BuildContext context) {
@@ -32,62 +32,69 @@ class _PeriodDurationScreenState extends State<PeriodDurationScreen> {
         ),
         title: Text('Setup Your Profile', style: TextStyle(fontWeight: FontWeight.bold)),
         centerTitle: true,
-        backgroundColor: Color.fromRGBO(230, 230, 250, 1),
+        backgroundColor: Color.fromRGBO(230, 230, 250, 100),
       ),
       body: Material(
-        color: Color.fromRGBO(230, 230, 250, 1),
+        color: Color.fromRGBO(230, 230, 250, 100),
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
             children: [
               SizedBox(height: 20),
-              StepProgressIndicator(currentStep: 5),
-              SizedBox(height: 20), 
+              StepProgressIndicator(currentStep: 7),
+              SizedBox(height: 30),
               Text(
-                'How long does your period usually last?',
-                textAlign: TextAlign.center,
+                'When was your last period?',
                 style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
               ),
               SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Expanded(
-                    child: CupertinoPicker(
-                      itemExtent: 50,
-                      onSelectedItemChanged: (index) {
-                        setState(() {
-                          selectedPeriodLength = index + 1;
-                        });
-                      },
-                      children: List.generate(duration.length, (index) {
-                        return Center(child: Text('${duration[index]} days')); 
-                      }),
-                    ),
+              // Calendar Widget
+              TableCalendar(
+                firstDay: DateTime.utc(2010, 1, 1),
+                lastDay: DateTime.utc(2030, 12, 31),
+                focusedDay: _focusedDay,
+                calendarFormat: CalendarFormat.month,
+                selectedDayPredicate: (day) {
+                  return _selectedDays.contains(day);
+                },
+                onDaySelected: (selectedDay, focusedDay) {
+                  setState(() {
+                    if (_selectedDays.contains(selectedDay)) {
+                      _selectedDays.remove(selectedDay);
+                    } else {
+                      _selectedDays.add(selectedDay);
+                    }
+                    _focusedDay = focusedDay; 
+                  });
+                },
+                calendarStyle: CalendarStyle(
+                  isTodayHighlighted: true,
+                  selectedDecoration: BoxDecoration(
+                    color: Colors.redAccent,
+                    shape: BoxShape.circle,
                   ),
-                ],
+                ),
               ),
               Spacer(),
               SizedBox(
                 width: double.infinity,
                 child: ElevatedButton(
                   onPressed: () async {
-                    List<dynamic> periodData = [selectedPeriodLength, widget.name];
-
-                    CSVManager csvManager = CSVManager();
-                    await csvManager.addToCSV(periodData);
-
+                    List<String> periodDates = _selectedDays.map((day) => day.toIso8601String()).toList();
+                    
+                    await csvManager.addToCSV([widget.name, '', '', '', '', '', '', '', periodDates.join(', ')]);
+                    
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                        builder: (context) => CycleLengthScreen(name: widget.name),
+                        builder: (context) => FinishSetupScreen(name: widget.name, userData: userData,),
                       ),
                     );
                   },
                   style: ElevatedButton.styleFrom(
-                    backgroundColor: Color.fromRGBO(255, 111, 97, 1),
+                    backgroundColor: Color.fromRGBO(255, 111, 97, 100),
                   ),
-                  child: Text('Next', style: TextStyle(color: Colors.white70)),
+                  child: Text('Next'),
                 ),
               ),
             ],
