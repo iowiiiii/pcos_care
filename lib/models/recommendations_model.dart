73c7f78ac5a84c66155c631bd4659322311a4175
ABO <140602836+AB0-0@users.noreply.github.com>
2024-10-21 13:10:05 +0800

    Update recommendations_model.dart

--- lib/models/recommendations_model.dart
+++ lib/models/recommendations_model.dart
@@ -1,32 +1,11 @@
 class RecommendationItem {
   final String title;
-  final String? author;
   final String? buttonLabel;
-  final String? link;
+  final String detail;
 
   RecommendationItem({
     required this.title,
-    this.author,
     this.buttonLabel,
-    this.link,
+    required this.detail,
   });
-
-  Map<String, dynamic> toJson(){
-    return {
-      'title': title,
-      'author': author,
-      'buttonLabel': buttonLabel,
-      'link': link,
-    };
-  }
-
-  factory RecommendationItem.fromJson(Map<String, dynamic> json) {
-    return RecommendationItem(
-      title: json['title'],
-      author: json['author'],
-      buttonLabel: json['buttonLabel'],
-      link: json['link'],
-    );
-}
-
 }
